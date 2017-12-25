2731f91a33e754556e9401c0882b8754ff0df12a
Ryo Manzoku <manzoku.ryo@gmail.com>
2017-12-25 07:03:32 +0000

    Fix test

--- lib/puma_test.go
+++ lib/puma_test.go
@@ -3,24 +3,26 @@ package mppuma
 import "testing"
 
 func TestGraphDefinition(t *testing.T) {
+	desired := 4
 
 	var puma PumaPlugin
 
 	graphdef := puma.GraphDefinition()
 
-	if len(graphdef) != 2 {
-		t.Errorf("GraphDefinition: %d should be %d", len(graphdef), 2)
+	if len(graphdef) != desired {
+		t.Errorf("GraphDefinition: %d should be %d", len(graphdef), desired)
 	}
 }
 
 func TestGraphDefinitionWithGC(t *testing.T) {
+	desired := 8
 
 	var puma PumaPlugin
 	puma.WithGC = true
 
 	graphdef := puma.GraphDefinition()
 
-	if len(graphdef) != 3 {
-		t.Errorf("GraphDefinitionWithGC: %d should be %d", len(graphdef), 3)
+	if len(graphdef) != desired {
+		t.Errorf("GraphDefinitionWithGC: %d should be %d", len(graphdef), desired)
 	}
 }
